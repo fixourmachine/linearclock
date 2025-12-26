e461ea39686c5b78157fa302946b91082fe5e6ea
fixourmachine <158464968+fixourmachine@users.noreply.github.com>
2025-12-26 01:55:45 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'linear-clock-v1';
+const ASSETS_TO_CACHE = [
+  './linear_clock.html',
+  './manifest.json',
+  // Add your icon filenames here if you have them
+  // './icon-192.png',
+  // './icon-512.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate Event (Clean up old caches)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
